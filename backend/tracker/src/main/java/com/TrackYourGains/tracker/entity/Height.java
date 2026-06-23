f12b77ee68ef6267385f49883122a832e5b07d03
Srivathsa Mantrala <srivathsamv2@gmail.com>
2026-06-23 14:43:35 -0400

    #1 Added user entity with fields

---
+++ backend/tracker/src/main/java/com/TrackYourGains/tracker/entity/Height.java
@@ -0,0 +1,20 @@
+package com.TrackYourGains.tracker.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Embeddable;
+import lombok.Getter;
+import lombok.Setter;
+
+@Embeddable
+@Getter
+@Setter
+public class Height {
+
+    /** Height in feet */
+    @Column(name = "height_feet")
+    private int feet;
+
+    /** Height in inches */
+    @Column(name = "heaight_inches")
+    private int  inches;
+}
---
+++ backend/tracker/src/main/java/com/TrackYourGains/tracker/entity/User.java
@@ -0,0 +1,32 @@
+package com.TrackYourGains.tracker.entity;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Entity
+@Data
+@Table(name = "Users")
+public class User {
+
+    /** ID of the user */
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "ID")
+    private Long id;
+
+    /** first name of the user */
+    @Column(name = "first_name", nullable = false)
+    private String firstName;
+
+    /** last name of the user */
+    @Column(name = "last_name", nullable = false)
+    private String lastName;
+
+    /** field to store the weight of the user */
+    @Column(name = "user_weight")
+    private Double weight;
+
+    /** Storing the height of the user as an embedded field */
+    @Embedded
+    private Height height;
+}
