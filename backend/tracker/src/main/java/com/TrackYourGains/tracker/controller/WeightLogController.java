bd934a37018a4648231db60b09f64d194c8241d6
Srivathsa Mantrala <srivathsamv2@gmail.com>
2026-06-27 09:19:16 -0400

    #2 Added a weight log controller to update weight independently

---
+++ backend/tracker/src/main/java/com/TrackYourGains/tracker/controller/WeightLogController.java
@@ -0,0 +1,45 @@
+package com.TrackYourGains.tracker.controller;
+
+import com.TrackYourGains.tracker.dto.WeightLogDto;
+import com.TrackYourGains.tracker.entity.WeightLog;
+import com.TrackYourGains.tracker.repository.UserRepository;
+import com.TrackYourGains.tracker.repository.WeightLogRepository;
+import com.TrackYourGains.tracker.service.WeightLogService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("weight-logs")
+public class WeightLogController {
+
+    @Autowired
+    private WeightLogService weightLogService;
+
+    @Autowired
+    private WeightLogRepository weightLogRepository;
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @GetMapping("/{id}")
+    public ResponseEntity<List<WeightLog>> getAllWeightLogs(@PathVariable Long id) {
+        if(userRepository.findById(id).isEmpty())
+            return new ResponseEntity("User does not exist!", HttpStatus.NOT_FOUND);
+        return ResponseEntity.ok(weightLogRepository.findByUserId(id));
+    }
+
+    @PostMapping("/{id}")
+    public ResponseEntity<WeightLogDto> addWeightLog(@PathVariable Long id, @RequestBody double request) {
+        if(userRepository.findById(id).isEmpty())
+            return new ResponseEntity("User does not exist!", HttpStatus.NOT_FOUND);
+        WeightLogDto weightLogDto = new WeightLogDto();
+        weightLogDto.setUserId(id);
+        weightLogDto.setWeight(request);
+        weightLogDto.setMessage("User updated their weight!");
+        return ResponseEntity.ok(weightLogService.logWeight(id, weightLogDto));
+    }
+}
